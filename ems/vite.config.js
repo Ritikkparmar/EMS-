2d3272de7566f1f8bb0af3fd2f5fb56510b498f2
Ritikkparmar <ritikparmar255@gmail.com>
2025-01-30 00:00:47 +0530

    Fix Vercel 404 issue

--- ems/vite.config.js
+++ ems/vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: './',  // Use './' to make paths relative or set it to '/EMS-/' if using a custom domain
   plugins: [react()],
-  base: '/EMS-/' // Add your GitHub repo name
 });
